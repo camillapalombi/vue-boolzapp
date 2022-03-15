0b4441f6a0911617589e7fb3298d905470057c99
camillapalombi <camilla.palombi14@gmail.com>
2022-03-15 12:05:09 +0100

    Predisposta struttura dati JS

---
+++ js/script.js
@@ -0,0 +1,41 @@
+//MILESTONE 1.
+
+const app = new Vue({
+    el : '#root',
+    data : {
+        arrUsers : [
+            {
+                name: 'Michele',
+                image: 'avatar_1.jpg'
+            },
+            {
+                name: 'Fabio',
+                image: 'avatar_2.jpg'
+            },
+            {
+                name: 'Samuele',
+                image: 'avatar_3.jpg'
+            },
+            {
+                name: 'Alessandro B.',
+                image: 'avatar_4.jpg'
+            },
+            {
+                name: 'Alessandro L.',
+                image: 'avatar_5.jpg'
+            },
+            {
+                name: 'Claudia',
+                image: 'avatar_6.jpg'
+            },
+            {
+                name: 'Federico',
+                image: 'avatar_7.jpg'
+            },
+            {
+                name: 'Davide',
+                image: 'avatar_8.jpg'
+            },
+        ]
+    }
+});
