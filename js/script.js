b17a6a91b33c62035fb361aa0fb4b6e5f3dac591
camillapalombi <camilla.palombi14@gmail.com>
2022-03-17 19:05:34 +0100

    Cancellazione messaggi

--- js/script.js
+++ js/script.js
@@ -13,17 +13,20 @@ const app = new Vue({
                     {
                         text: 'Ciao, come stai?',
                         date: `${luxon.DateTime.fromISO('2022-03-16T09:43').toRelativeCalendar()} alle 9:43`,
-                        status: 'inviato'
+                        status: 'inviato',
+                        opendrop: true
                     },
                     {
                         text: 'Io mi sto riprendendo dal covid ancora..',
                         date: `${luxon.DateTime.fromISO('2022-03-16T09:47').toRelativeCalendar()} alle 9:47`,
-                        status: 'inviato'
+                        status: 'inviato',
+                        opendrop: true
                     },
                     {
                         text: 'Ei ciao, io abbastanza bene dai. Dai che tra poco starai meglio!',
                         date: `${luxon.DateTime.fromISO('2022-03-16T09:53').toRelativeCalendar()} alle 9:53`,
-                        status: 'ricevuto'
+                        status: 'ricevuto',
+                        opendrop: true
                     }
                 ],
             },
@@ -35,17 +38,20 @@ const app = new Vue({
                     {
                         text: 'Hai tolto i panni dalla lavatrice??',
                         date: `${luxon.DateTime.fromISO('2022-03-07T18:13').toRelativeCalendar()} alle 18:13`,
-                        status: 'inviato'
+                        status: 'inviato',
+                        opendrop: true
                     },
                     {
                         text: 'Si amore, tranquilla!',
                         date: `${luxon.DateTime.fromISO('2022-03-07T18:33').toRelativeCalendar()} alle 18:33`,
-                        status: 'ricevuto'
+                        status: 'ricevuto',
+                        opendrop: true
                     },
                     {
                         text: 'Bravo tesoro, a dopo! :)',
                         date: `${luxon.DateTime.fromISO('2022-03-07T18:43').toRelativeCalendar()} alle 18:43`,
-                        status: 'inviato'
+                        status: 'inviato',
+                        opendrop: true
                     }
                 ],
             },
@@ -57,17 +63,20 @@ const app = new Vue({
                     {
                         text: 'Mi ricordi la password di Netflix?',
                         date: `${luxon.DateTime.fromISO('2022-03-10T09:01').toRelativeCalendar()} alle 9:01`,
-                        status: 'inviato'
+                        status: 'inviato',
+                        opendrop: true
                     },
                     {
                         text: 'Me la sono scordata...',
                         date: `${luxon.DateTime.fromISO('2022-03-10T09:16').toRelativeCalendar()} alle 9:16`,
-                        status: 'inviato'
+                        status: 'inviato',
+                        opendrop: true
                     },
                     {
                         text: 'Sempre la solita, eccola: 2345672',
                         date: `${luxon.DateTime.fromISO('2022-03-10T09:43').toRelativeCalendar()} alle 9:43`,
-                        status: 'ricevuto'
+                        status: 'ricevuto',
+                        opendrop: true
                     }
                 ],
             },
@@ -79,17 +88,20 @@ const app = new Vue({
                     {
                         text: 'Lo porti tu il bambino a calcio?',
                         date: `${luxon.DateTime.fromISO('2022-03-02T09:41').toRelativeCalendar()} alle 9:41`,
-                        status: 'inviato'
+                        status: 'inviato',
+                        opendrop: true
                     },
                     {
                         text: 'Non ho tempo.',
                         date: `${luxon.DateTime.fromISO('2022-03-02T09:43').toRelativeCalendar()} alle 9:43`,
-                        status: 'ricevuto'
+                        status: 'ricevuto',
+                        opendrop: true
                     },
                     {
                         text: 'Grazie..',
                         date: `${luxon.DateTime.fromISO('2022-03-02T20:43').toRelativeCalendar()} alle 20:43`,
-                        status: 'inviato'
+                        status: 'inviato',
+                        opendrop: true
                     }
                 ],
             },
@@ -101,17 +113,20 @@ const app = new Vue({
                     {
                         text: 'Mi passi gli appunti di storia?',
                         date: `${luxon.DateTime.fromISO('2022-03-14T09:15').toRelativeCalendar()} alle 9:15`,
-                        status: 'ricevuto'
+                        status: 'ricevuto',
+                        opendrop: true
                     },
                     {
                         text: 'No mi dispiace..',
                         date: `${luxon.DateTime.fromISO('2022-03-14T09:20').toRelativeCalendar()} alle 9:20`,
-                        status: 'inviato'
+                        status: 'inviato',
+                        opendrop: true
                     },
                     {
                         text: 'Fatteli da solo come ho fatto io',
                         date: `${luxon.DateTime.fromISO('2022-03-14T09:43').toRelativeCalendar()} alle 9:43`,
-                        status: 'inviato'
+                        status: 'inviato',
+                        opendrop: true
                     }
                 ],
             },
@@ -123,17 +138,20 @@ const app = new Vue({
                     {
                         text: 'Cosa vuoi per cena?',
                         date: `${luxon.DateTime.fromISO('2022-03-11T09:20').toRelativeCalendar()} alle 9:20`,
-                        status: 'inviato'
+                        status: 'inviato',
+                        opendrop: true
                     },
                     {
                         text: 'Tagliata di manzo',
                         date: `${luxon.DateTime.fromISO('2022-03-11T09:31').toRelativeCalendar()} alle 9:31`,
-                        status: 'ricevuto'
+                        status: 'ricevuto',
+                        opendrop: true
                     },
                     {
                         text: 'Sarà fatto, a dopo.',
                         date: `${luxon.DateTime.fromISO('2022-03-11T09:43').toRelativeCalendar()} alle 9:43`,
-                        status: 'inviato'
+                        status: 'inviato',
+                        opendrop: true
                     }
                 ],
             },
@@ -145,12 +163,14 @@ const app = new Vue({
                     {
                         text: 'hai rotto tu la mia collana?',
                         date: `${luxon.DateTime.fromISO('2022-03-15T09:43').toRelativeCalendar()} alle 9:20`,
-                        status: 'inviato'
+                        status: 'inviato',
+                        opendrop: true
                     },
                     {
                         text: 'ma cosa dici.. NO!',
                         date: `${luxon.DateTime.fromISO('2022-03-15T09:43').toRelativeCalendar()} alle 9:43`,
-                        status: 'ricevuto'
+                        status: 'ricevuto',
+                        opendrop: true
                     }
                 ],
             },
@@ -162,16 +182,18 @@ const app = new Vue({
                     {
                         text: 'Tanti auguri di buon compleanno Dave!!',
                         date: `${luxon.DateTime.fromISO('2022-03-16T09:32').toRelativeCalendar()} alle 9:32`,
-                        status: 'inviato'
+                        status: 'inviato',
+                        opendrop: true
                     },
                     {
                         text: 'Grazie mille',
                         date: `${luxon.DateTime.fromISO('2022-03-16T09:43').toRelativeCalendar()} alle 9:43`,
-                        status: 'ricevuto'
+                        status: 'ricevuto',
+                        opendrop: true
                     },
                 ],
             },
-        ]
+        ],
     },
     methods : {
         //Funzione invio messaggio
@@ -182,7 +204,8 @@ const app = new Vue({
                 {
                     text: this.newMessage, //testo inserito nell'input
                     date:  `${luxon.DateTime.now().toRelativeCalendar()} alle ${luxon.DateTime.now().toFormat('HH:mm')}`, // oggi alle
-                    status: 'inviato'
+                    status: 'inviato',
+                    opendrop: true
                 }
             );
             this.newMessage = ''; //azzeramento campo input
@@ -192,7 +215,8 @@ const app = new Vue({
                     {
                         date: `${luxon.DateTime.now().toRelativeCalendar()} alle ${luxon.DateTime.now().toFormat('HH:mm')}`, //oggi alle
                         text: "Okkei",
-                        status: 'ricevuto'
+                        status: 'ricevuto',
+                        opendrop: true
                     }
                 );
             }
@@ -203,14 +227,26 @@ const app = new Vue({
         showChat(index) {
             this.active = index;
         },
+        //ricerca tramite input delle mie chat
         findUserChat() {
-            this.arrUsers.forEach(element => {
-                if (element.name.toLowerCase().includes(this.findString.toLowerCase())) {
+            this.arrUsers.forEach(element => { //per ogni elemento nel mio array:
+                if (element.name.toLowerCase().includes(this.findString.toLowerCase())) { //se il nome dell'user contiene lettere contenute nel campo input(findString) fai:
                     element.visible = true;
-                } else {
+                } else { //se non hanno lettere in comune fai:
                     element.visible = false;
                 }
             });
+        },
+        deleteMessage(active) {
+            this.arrUsers[active].chat.splice(-1, 1);
         }
     }
 });
+
+
+
+
+/*deleteMessage(index) {
+            let indexMsg = this.arrUsers[this.active].chat;
+                indexMsg.splice(index, 1);
+        },*/
